157627fe685e4f9d3f43f90625bd0d754d5234f0
arianne-toepfer <78578889+arianne-toepfer@users.noreply.github.com>
2021-06-19 15:53:38 -0700

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -25,20 +25,27 @@ form.on("submit",runEnter);
 
 // Complete the event handler function for the form
 function runEnter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
 
-// Prevent the page from refreshing
-d3.event.preventDefault();
-  
-// Select the input element and get the raw HTML node
-var inputElement = d3.select("#datetime");
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
 
-// Get the value property of the input element
-var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    //console.log(tableData);
 
-console.log(inputValue);
-console.log(tableData);
+    var filteredDate = tableData.filter(date => date.datetime === inputValue);
 
-var filteredDate = tableData.filter(date => tableData.datetime === inputValue);
+    console.log(filteredDate);
 
-console.log(filteredDate);
+    //filteredDate.forEach((ufoSiting) => {
+        //var row = tbody.append("tr");
+        //Object.entries(ufoSiting).forEach(([key, value]) => {
+          //var cell = row.append("td");
+          //cell.text(value);
+        //});
+    //});
 };
