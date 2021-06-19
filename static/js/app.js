99f76e421441a2edbebac827ab45eac8e83b671f
arianne-toepfer <78578889+arianne-toepfer@users.noreply.github.com>
2021-06-19 15:11:58 -0700

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -12,4 +12,33 @@ tableData.forEach((ufoSiting) => {
   });
 });
 
+//add Filter to look for specific date and return the result
+// Select the button
+var button = d3.select("#button");
 
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+// Prevent the page from refreshing
+d3.event.preventDefault();
+  
+// Select the input element and get the raw HTML node
+var inputElement = d3.select("#datetime");
+
+// Get the value property of the input element
+var inputValue = inputElement.property("value");
+
+console.log(inputValue);
+console.log(tableData);
+
+var filteredDate = tableData.filter(date => tableData.datetime === inputValue);
+
+console.log(filteredDate);
+};
